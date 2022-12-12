d6a525c42e33bf89d3dd4275304e2b960859057d
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-12 15:34:43 -0300

    update

---
+++ tutorial-react-route-dom/src/components/Post/index.tsx
@@ -0,0 +1,8 @@
+export const Post = () => {
+    return(
+        <div>
+            <h1>Post</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur sequi praesentium ab, obcaecati accusantium modi laboriosam dolor iure, atque quas deserunt quisquam recusandae cumque sit aperiam tempore, vitae dicta fuga!</p>
+        </div>
+    );
+};
---
+++ tutorial-react-route-dom/src/components/Posts/index.tsx
@@ -0,0 +1,14 @@
+import { useParams, useSearchParams, Outlet } from 'react-router-dom';
+
+export const Posts = () => {
+    const params = useParams();
+    const { id } = params;
+    const [ qs ] = useSearchParams()
+
+    return(
+        <div>
+            <h1>Post {`Para: ${id} `} {`QS: ${qs}`}</h1>
+            <Outlet/>
+        </div>
+    );
+};
